5afab1c1b2d6066c6f2c488062c590fe9d6ca81b
PejmanAslani <aslanipejman79@gmail.com>
2023-03-08 16:19:33 +0330

    4

--- src/componnets/call-routes/AddActions/AddActions.tsx
+++ src/componnets/call-routes/AddActions/AddActions.tsx
@@ -3,98 +3,53 @@ import React from "react";
 import { Button, Row, Col } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import ToolTipCustom from "../../reuseables/tooltip/ToolTipCustom";
-import { PlineActions } from "../../services/PlineActions";
+import { IPlineActions, PlineActions } from "../../services/PlineActions";
 import ModalCustom from "../../reuseables/modal/ModalCustom";
 import ActionsForm from "./ActionsForm";
 import PlineTools from "../../services/PlineTools";
 
+
 function AddActions(props: any) {
+
     var data = Object.values(PlineActions).filter(value => typeof value === 'string');
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [modaltype, setmodalType] = useState({});
-    const [sizeModal, setSizeModal] = useState("");
-    const [state, setState] = useState({
-        action: "",
-        options: []
+    const [state, setState] = useState<IPlineActions>({
+        id: props.id,
+        action: PlineActions.SIPUser,
+        value: null,
+        options: null,
     });
-    const setActionType = (e: any) => {
+
+    const setActionType = (e: PlineActions) => {
         switch (e) {
-            case "0":
-                getActionLists(0);
-                setSizeModal("lg");
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm data={state} title="Sip User" type="text" label="Sip User" name="sipUser" />
-                );
+            case PlineActions.SIPUser:
+
                 break;
-            case "1":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm title="Ring Group" type="list" />
-                );
+            case PlineActions.RingGroup:
+
                 break;
-            case "2":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm title="IVR" type="list" />
-                );
+            case PlineActions.IVR:
+
                 break;
-            case "3":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm title="Queue" type="list" />
-                );
+            case PlineActions.Queue:
+
                 break;
-            case "4":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm title="Time Condition" type="time" />
-                );
+            case PlineActions.TimeCondition:
+
                 break;
-            case "5":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm
-                        title="Announcement"
-                        type="list"
-                    />
-                );
+            case PlineActions.Announcement:
+
                 break;
-            case "6":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm
-                        title="SIP Trunk Transit"
-                        type="list"
-                    />
-                );
+            case PlineActions.SIPTrunkTransit:
+
                 break;
-            case "7":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm
-                        title="Call On SIP Trunk"
-                        type="list"
-                    />
-                )
+            case PlineActions.CallOnSIPTrunk:
+
                 break;
-            case "8":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm
-                        title="Direct Dial"
-                        type="text2"
-                    />
-                );
+            case PlineActions.DirectDial:
+
                 break;
-            case "9":
-                setModalIsOpen(true);
-                setmodalType(
-                    <ActionsForm
-                        title="API Interface"
-                        type="list"
-                    />
-                );
+            case PlineActions.APIInterface:
+
                 break;
         }
     }
@@ -110,15 +65,7 @@ function AddActions(props: any) {
         <div className="container">
             <h3> Add Action</h3>
             <hr />
-            <Row>
-                <ModalCustom
-                    size={sizeModal}
-                    show={modalIsOpen}
-                    onHide={() => setModalIsOpen(false)}
-                >
-                    {modaltype}
-                </ModalCustom>
-            </Row>
+
             <Form>
                 <Row>
                     <Col md={6}>
@@ -131,7 +78,7 @@ function AddActions(props: any) {
                                     setActionType(e.target.value)
                                 }}
                             >
-                                <option value={0}>Select Action Type</option>
+                                <option value={10}>Select Action Type</option>
                                 {data.map((key: any, value: any) => (
                                     <option key={key} value={value}>
                                         {key}
@@ -142,12 +89,7 @@ function AddActions(props: any) {
                     </Col>
                     <Row>
                         <Col>
-                            <Button
-                                variant="primary"
-                                onClick={() => {
-
-                                }}
-                            >
+                            <Button variant="primary" onClick={() => { }}>
                                 Save
                             </Button>
                             {" "}
